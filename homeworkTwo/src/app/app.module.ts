a2a56a061fe5c24aa1aff980962d54c143cae8cf
orangutan11 <orangutan11112000@gmail.com>
2024-07-25 14:10:03 +0700

    update : homeworkTwo 2.2

--- homeworkTwo/src/app/app.module.ts
+++ homeworkTwo/src/app/app.module.ts
@@ -5,15 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VdtPageComponent } from './vdt-page/vdt-page.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
+import { en_US, th_TH} from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+import th from '@angular/common/locales/th';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+// add component
+import { NewComponent } from './new/new.component';
+import { VatComponent } from './vat/vat.component';
 
 // add
 import { NzGridModule } from 'ng-zorro-antd/grid';
@@ -28,12 +33,26 @@ import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import { NzMessageModule } from 'ng-zorro-antd/message';
 import { NzNotificationModule } from 'ng-zorro-antd/notification';
 
-registerLocaleData(en);
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { NzImageModule } from 'ng-zorro-antd/image';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+
+
+// registerLocaleData(en);
+registerLocaleData(th);
 
 @NgModule({
   declarations: [
     AppComponent,
-    VdtPageComponent
+    VdtPageComponent,
+    VatComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
@@ -54,9 +73,18 @@ registerLocaleData(en);
     NzModalModule,
     NzPopconfirmModule,
     NzMessageModule,
-    NzNotificationModule
+    NzNotificationModule,
+    NzBreadCrumbModule,
+    NzUploadModule,
+    NzCheckboxModule,
+    NzRadioModule,
+    NzAvatarModule,
+    NzPaginationModule,
+    NzImageModule,
+    NzDividerModule,
+    NzInputNumberModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  providers: [{ provide: NZ_I18N, useValue: th_TH }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
