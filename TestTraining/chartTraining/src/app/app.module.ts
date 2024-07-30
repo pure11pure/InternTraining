23a0951e0852ce21f9d2ba04108f29b3c0098219
orangutan11 <orangutan11112000@gmail.com>
2024-07-30 11:09:45 +0700

    update : chart1, chart2, chart3

--- TestTraining/chartTraining/src/app/app.module.ts
+++ TestTraining/chartTraining/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgApexchartsModule } from 'ng-apexcharts';
+import { Chart1Component } from './chart1/chart1.component';
+import { Chart2Component } from './chart2/chart2.component';
+import { Chart3Component } from './chart3/chart3.component';
 // import * as ApexCharts from 'apexcharts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Chart1Component,
+    Chart2Component,
+    Chart3Component
   ],
   imports: [
     BrowserModule,
---
+++ TestTraining/chartTraining/src/app/chart3/chart3.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import {
+  ApexNonAxisChartSeries,
+  ApexPlotOptions,
+  ApexChart,
+  ApexLegend,
+  ApexResponsive,
+  ChartComponent
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  labels: string[];
+  colors: string[];
+  legend: ApexLegend;
+  plotOptions: ApexPlotOptions;
+  responsive: ApexResponsive[];
+};
+
+@Component({
+  selector: 'app-chart3',
+  templateUrl: './chart3.component.html',
+  styleUrls: ['./chart3.component.scss']
+})
+export class Chart3Component implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+
+  constructor() {
+    this.chartOptions = {
+      series: [76, 67, 61, 90],
+      chart: {
+        height: 390,
+        type: "radialBar"
+      },
+      plotOptions: {
+        radialBar: {
+          offsetY: 0,
+          startAngle: 0,
+          endAngle: 270,
+          hollow: {
+            margin: 5,
+            size: "30%",
+            background: "transparent",
+            image: undefined
+          },
+          dataLabels: {
+            name: {
+              show: false
+            },
+            value: {
+              show: false
+            }
+          }
+        }
+      },
+      colors: ["#1ab7ea", "#0084ff", "#39539E", "#0077B5"],
+      labels: ["Vimeo", "Messenger", "Facebook", "LinkedIn"],
+      legend: {
+        show: true,
+        floating: true,
+        fontSize: "16px",
+        position: "left",
+        offsetX: 50,
+        offsetY: 10,
+        labels: {
+          useSeriesColors: true
+        },
+        formatter: function (seriesName, opts) {
+          return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex];
+        },
+        itemMargin: {
+          horizontal: 3
+        }
+      },
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            legend: {
+              show: false
+            }
+          }
+        }
+      ]
+    };
+  }
+
+
+}
