f90b099b986b7727db2ad9dbaf4422cd12e9e0ee
orangutan11 <orangutan11112000@gmail.com>
2024-09-10 15:12:18 +0700

    update : Suspense

--- NextJs/next-study/src/middleware.ts
+++ NextJs/next-study/src/middleware.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     console.log('|   middleware running for', request.url)
-    return NextResponse.redirect(new URL('/', request.url))
+    // return NextResponse.redirect(new URL('/', request.url))
+    return NextResponse.next()
 }
 
 // See "Matching Paths" below to learn more
