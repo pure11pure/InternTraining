2140fdc715cfe78a80ea8d9a9f7add894fd87119
orangutan11 <orangutan11112000@gmail.com>
2024-09-26 10:23:01 +0700

    update : useEffect II

--- react/todo/src/components/Video.jsx
+++ react/todo/src/components/Video.jsx
@@ -3,6 +3,8 @@ import { useRef, useEffect } from "react";
 export default function VideoPlayer({ src, isPlaying }) {
   const ref = useRef(null);
 
+  //   useEffect จะทำงานทุกรอบที่ isPlaying มีการเปลี่ยนแปลง
+  //   แต่ก็มีการดักจับ event การเปลี่ยนแปลง
   useEffect(() => {
     // ต้องการใข้ ref หลังจากที่ component render ออกมาแล้ว
     if (isPlaying) {
@@ -10,7 +12,16 @@ export default function VideoPlayer({ src, isPlaying }) {
     } else {
       ref.current.pause();
     }
-  });
+  }, [isPlaying]);
+
+  //   และถ้าต้องการให้ run แค่ครั้งเดียว ให้ใช้เป็น []
+  //   useEffect(() => {
+  //     if (isPlaying) {
+  //       ref.current.play();
+  //     } else {
+  //       ref.current.pause();
+  //     }
+  //   }, []);
 
   return <video ref={ref} src={src} loop playsInline />;
 }
