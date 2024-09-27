6cdfd06eea38de5342a88f7545f5a2bc3bd57ef9
orangutan11 <orangutan11112000@gmail.com>
2024-09-27 12:25:48 +0700

    start : strapi

---
+++ strapi/src/api/special-blog/controllers/special-blog.ts
@@ -0,0 +1,7 @@
+/**
+ * special-blog controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::special-blog.special-blog');
---
+++ strapi/src/api/special-blog/routes/special-blog.ts
@@ -0,0 +1,7 @@
+/**
+ * special-blog router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::special-blog.special-blog');
