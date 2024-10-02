aeb532a143d79445ed542358a20e319f5bbf6369
orangutan11 <orangutan11112000@gmail.com>
2024-10-02 16:11:48 +0700

    update :  react+vitest (unit test)2

--- react-vitest (unit testing)/react-test-app/src/App.jsx
+++ react-vitest (unit testing)/react-test-app/src/App.jsx
@@ -1,20 +1,26 @@
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
+import UserList from "./components/UserList";
 
 function App() {
   return (
     <>
-      <div>Component Counter</div>
-      <Counter/>
+      {/* <div>Component Counter</div>
+      <Counter /> */}
+      <div>UserList Component</div>
+      <UserList />
     </>
   );
 }
 
 export default App;
 
-
 /**
- * เวลาจะ Test 
+ * TODO: เวลาจะ Test Counter
  * - component render ได้ไหม
  * - เพิ่มได้
  * - ลดได้
+ * 
+ *  TODO: เวลาจะ Test UserList
+ * - ดึงข้อมูล
+ * - หาข้อมูลผ่านชื่อ และ email
  */
