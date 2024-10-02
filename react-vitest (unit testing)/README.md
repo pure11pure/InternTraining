2c3ccf1a541dcd55a50aeff12322fe3d052e1c32
orangutan11 <orangutan11112000@gmail.com>
2024-10-02 14:39:54 +0700

    start : react+vitest (unit test)

---
+++ react-vitest (unit testing)/README.md
@@ -0,0 +1,12 @@
+## Unit Test
+
+- [https://vitest.dev/]
+- [https://testing-library.com/docs/queries/about]
+
+## Command
+
+### 1. run test (change reload)
+
+```
+ npx vitest
+```
---
+++ react-vitest (unit testing)/react-test-app/src/App.jsx
@@ -0,0 +1,20 @@
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      <div>Component Counter</div>
+      <Counter/>
+    </>
+  );
+}
+
+export default App;
+
+
+/**
+ * เวลาจะ Test 
+ * - component render ได้ไหม
+ * - เพิ่มได้
+ * - ลดได้
+ */
