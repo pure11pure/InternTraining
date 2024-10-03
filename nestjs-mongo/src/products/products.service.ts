38900fc0f15bb3180be810515c6b5e4f85f7a9fa
pure11pure <orangutan11112000@gmail.com>
2024-10-03 15:27:08 +0700

    update : connect mongo

--- nestjs-mongo/src/products/products.service.ts
+++ nestjs-mongo/src/products/products.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
-  private readonly products = [
-    { id: 1, name: 'product 1', description: 'description 1' },
-    { id: 2, name: 'product 2', description: 'description 2' },
-  ];
-
-  findAll() {
-    return this.products;
+  create(createProductDto: CreateProductDto) {
+    return 'This action adds a new product';
   }
 
-  create(product) {
-    this.products.push(product);
-    return product;
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
   }
 }
